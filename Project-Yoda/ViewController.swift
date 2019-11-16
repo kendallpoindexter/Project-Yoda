b7cf7dc0db17f8b537672cb8e5b1be7c9f8e2746
Kendall <kendall.poindexter@detroitlabs.com>
2019-11-16 14:28:20 -0500

    intial commit

---
+++ Project-Yoda/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project-Yoda
+//
+//  Created by Kendall Poindexter on 11/16/19.
+//  Copyright © 2019 Kendall Poindexter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project-YodaTests/Project_YodaTests.swift
@@ -0,0 +1,34 @@
+//
+//  Project_YodaTests.swift
+//  Project-YodaTests
+//
+//  Created by Kendall Poindexter on 11/16/19.
+//  Copyright © 2019 Kendall Poindexter. All rights reserved.
+//
+
+import XCTest
+@testable import Project_Yoda
+
+class Project_YodaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
